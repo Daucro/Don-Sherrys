7134b330cb928208594b1404c85b1eeea6cbc118
Daucro <142437961+Daucro@users.noreply.github.com>
2023-09-26 11:57:50 -0500

    Update

--- don-sherrys/src/app/components/navigation.tsx
+++ don-sherrys/src/app/components/navigation.tsx
@@ -1,34 +1,40 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import NavButton from './navbutton';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import NavButton from "./navbutton";
 
 export default function Navigation() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{display:'flex',justifyContent:'space-between',backgroundColor:'blue'}}>
+      <AppBar position='static'>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            backgroundColor: "blue",
+          }}
+        >
           <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
             sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Box sx={{display:'flex'}}>
-            <NavButton text="Home" link='home'/>
-            <NavButton text="About Us" link='about'/>
-            <NavButton text="Amenities" link='amenities'/>
-            <NavButton text="Location" link='location'/>
-            <NavButton text="Menu" link='menu'/>
-            <Button color="inherit">Login</Button>
+          <Box sx={{ display: "flex" }}>
+            <NavButton text='Home' link='home' />
+            <NavButton text='About Us' link='about' />
+            <NavButton text='Amenities' link='amenities' />
+            <NavButton text='Location' link='location' />
+            <NavButton text='Menu' link='menu' />
+            <Button color='inherit'>Login</Button>
           </Box>
         </Toolbar>
       </AppBar>
